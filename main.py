06d4cc8bd4fe460964c701fd828eb28ab34bf6cd
zaxoder <167690364+zaxoder@users.noreply.github.com>
2024-04-28 13:57:32 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 
 pygame.init()
 
+SCREEN_WEIDHT = 1600
+SCREEN_HEIGHT = 900
+
+screen = pygame.display.set_mode((SCREEN_WEIDHT, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Первая игра от Ивана", "Поехали")
+
 running = True
 while running:
     pass
